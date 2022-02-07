327c51e95b18e0b470ca614689e5c55836363b6c
kokopuffz <chanheeya@gmail.com>
2022-02-06 18:48:54 -0600

    save 1, try something different next one

--- script.js
+++ script.js
@@ -8,39 +8,73 @@ let choices = [0,1,2,3]
 let recordPress = []
 let gameOver=false
 
-let pressedKeys = { }
-document.addEventListener('keydown', e => pressedKeys[e.key] = true)
-document.addEventListener('keyup', e => pressedKeys[e.key] = false) 
-console.log(pressedKeys)
+// let pressedKeys = { }
+// document.addEventListener('keydown', e => pressedKeys[e.key] = true)
+// document.addEventListener('keyup', e => pressedKeys[e.key] = false) 
+// document.body.onkeyup = function(e){
+//   if(e.keyCode == 32) {
+//     console.log('spacebar biatches')
+//   }
+// }
+// console.log(pressedKeys)
+
+
 
 const questions = [
+  // let spaceBar = " "
   { 
     question: "Mac: emojis",
+    answerOp: {
+      Meta: false,
+      Shift: false,
+      spaceBar: false
+    },
     answer: ['Meta', 'Shift', ' '],
     answertext: "command-shift-space"
   },
 
   { 
     question: "Mac: copy current line",
-    answer: ['Meta', 'Shift', " "],
+    answerOp: {
+      Alt: false,
+      Shift: false,
+      ArrowDown: false
+    },
+    answer: ['Alt', 'Shift', "ArrowDown"],
     answertext: "option-shift-down"
   },
   
   {
     question: "Mac\Chrome Devtools: elements mouseover panel",
-    answer: ['Meta', 'Shift', " "],
+    answer: ['Command', 'Shift', "c"],
+    // answer: (pressedKeys.command && pressedKeys.shift && pressedKeys.spaceBar) ,
+    // first conditionals to check if its pressed//actually chck
+    answerOp: {
+        Command: false,
+        Shift: false,
+        c: false
+      },
     answertext: "command-shift-c"
   },
 
   {
     question: "Mac\VS: move current line up",
+    answerOp: {
+        Alt: false,
+        ArrowUp: false
+      },
     answer: ['Alt', 'ArrowUp'],
     answertext: "alt-up"
   }
 ]
 
-// function pressedkeys() {
+//go to answer, get array, plug each into pressed keys execute pressed keys
+
+// function movementHandler() {
+//   // console.log(e)
+//   const speed = 5
 //   console.log(pressedKeys)
+// //   // movementDisplay.innerText = 'X:' + hero.x + ' ' + 'Y:' + hero.y 
 //   if (ogre.alive) movementDisplay.innerText = `X:${hero.x} Y:${hero.y}` 
 // //   // conditional logic based on what key was pressed
 //   if (pressedKeys.a || pressedKeys.ArrowLeft) hero.x -= speed
@@ -48,33 +82,26 @@ const questions = [
 //   if (pressedKeys.s || pressedKeys.ArrowDown) hero.y += speed
 //   if (pressedKeys.w || pressedKeys.ArrowUp) hero.y -= speed
 // }
-//after button is pressed to start
 
 
 // goal #1 show pressed keys on screen
-//make pressedDisplay that also listens to keyup
-let pressedDisplay = () => document.addEventListener("keydown", function(event) {
-  const p = document.createElement("p");
-  p.textContent = `${event.key}`;
-  outPut.appendChild(p);
-
-  if (!gameOver) {
-    let pressedKeys = () => {
-      if 
-    }
-    recordPress.push(event.key)
-    console.log(recordPress)
-  } else {
-    return
+//make pressedDisplay that also listens to keyup** later
+let compareKeys = () => document.addEventListener("keydown", function(event) {
+  const p = document.createElement("p")
+  p.textContent = `${event.key}`
+  // outPut.appendChild(p)
+  if (event.key === ' '){
+    p.textContent = 'Spacebar'
   }
+  outPut.appendChild(p)
+  recordPress.push(event.key)
+  console.log(recordPress)
+  }
+    // fadeAway.classList.toggle(fade)
+, true)
 
-//   // fadeAway.classList.toggle(fade)
-}, true);
 
-function
-
-
-
+//take out question that was just used
 let usedQuestions = (num) => {
   const index = choices.indexOf(num)
   if (index > -1) {
@@ -82,57 +109,65 @@ let usedQuestions = (num) => {
   }
 }
 
+
 let randomQuestionIndex = () => {
   const index = Math.floor(Math.random() * choices.length) 
     return (choices[index])
 }
 
-let correctCheck = (press, currentAnswer) => {
-    for(let i = 0; i < currentAnswer.length, i++){
-    let correctPress = currentAnswer[i]
+// let check = (move, currentAnswer) => {
+//   for (let i = 0; i < currentAnswer.length; i++ ) {
+//     let a = move.sort 
+//     let b = currentAnswer.sort 
+//     console.log(a)
+//     console.log(b)
+//     if (a[i] != b[i]) {
+//       console.log('checkF')
+//       return false
+      
+//     } else {
+//       console.log('checkF')
+//       return true
+      
+//     }
+//   }
+// }
 
-    if (press.includes(currentAnswer[i])){
-      return false
-    }
-  }
-}
 // let checkInput = () {
 // checks if the inputs are correct === boolean
-}
-// question is displayed and page displays every keyboard press
-let questionDisplay = (questions, topDisplay) => {
-  for (let i = 0; i < questions.length; i++) {
-    answers = [];
-  }
-
-  topDisplay.innerText = questions[idx].question
-}
+// }
 
 
 function gameLoop() {
-  pressedDisplay()
   //get random index from choices
   let currentQuestion = randomQuestionIndex()
   //display question
   topDisplay.innerText = questions[currentQuestion].question
-  //take current question out of choices
+  //start saving keystroke/
+  compareKeys()
+  
+  
+
+  //check keystroke against answers
+  //if user is right take current question out of choices
   usedQuestions(currentQuestion)
+  console.log(choices)
   //get the answers
   let currentAnswer = questions[currentQuestion].answer
-  //loop through answer in case order doesn't matter
-
+  //check is press is included inside current answers
+}
 
   //compare pressed keys 
-  console.log(currentAnswer)
   
-  if (gameOver) {
-    return
-  }
+  
+  // if (gameOver) {
+  //   return
+  // }
 
   // clear the canvas and then render
   // clear the entire canvs from tp left to bottom right
   // pressedKeys()
-}
+
 
 
 
